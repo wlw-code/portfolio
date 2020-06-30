f31b411ebfec73c7ec3b9e021c1b41a467bd33b2
Will Lewis Williams <lumitedubbz@gmail.com>
2020-06-30 22:53:04 +0100

    vue-router

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>All about me</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <h1>Will Lewis Williams</h1>
+    <h2>Student programmer</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #1e1e1e;
+  margin-top: 30px;
+}
+</style>

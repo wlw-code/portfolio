836d7c8328afb0d0aebad42a6feaf3dc672ce58a
Will Lewis Williams <will.lewis.w@gmail.com>
2020-07-05 20:25:53 +0100

    more info on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,14 @@
   <div class="home">
     <h1>Will Lewis Williams</h1>
     <h2>Student programmer</h2>
-    <p>Hello world</p>
+    <h3>Welcome!</h3>
+    <p>Welcome to my portfolio site. Here you may find all information regarding who I am, what I do, what I bring to the table and how you may get in touch with me.</p>
+    <p>This site also contains a list of projects I have undertaken and some information about them.</p>
+    <br>
+    <p>If you'd like to see some visual representation of my work, you may continue scrolling. Alternatively, you can use the navigation bar at the top of the page
+      to learn more about me, to see the individual projects I've worked on or my contact details.</p>
+
+    <h1>[Under construction]</h1>
   </div>
 </template>
 
@@ -11,8 +18,9 @@ h1 {
   margin-top: 20px;
 }
 
-p {
+h3, p {
   text-align: left;
   padding: 30px;
+  padding-top: 0;
 }
 </style>
